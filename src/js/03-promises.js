cf04e00b1482d97da77fd6081d2e04aad4c90185
VitaBabenko <babenkovita1988@gmail.com>
2022-12-30 13:03:50 +0200

    done js3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,6 @@
+import Notiflix from 'notiflix';
+
+
 const form = document.querySelector('.form');
 const delay = document.querySelector('input[name="delay"]');
 const step = document.querySelector('input[name="step"]');
@@ -11,8 +14,12 @@ function onFormSubmit(evt) {
   for (let i = 0; i <= Number(amount.value); i += 1) {
     const delayStep = Number(delay.value) + Number(step.value) * (i - 1);
     createPromise(i, delayStep)
-      .then(({ position, delay }) => { console.log(`✅ Fulfilled promise ${position} in ${delay}ms`) })
-      .catch(({ position, delay }) => { console.log(`❌ Rejected promise ${position} in ${delay}ms`) })
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      })
   }
 }
 

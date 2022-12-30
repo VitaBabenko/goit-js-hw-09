0fd79811bec3706f42b3b977188a912810f32c23
VitaBabenko <babenkovita1988@gmail.com>
2022-12-30 14:40:52 +0200

    changed js2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,57 +1,57 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-console.dir(flatpickr)
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr("#datetime-picker", options);
 
 const input = document.querySelector('#datetime-picker');
 const button = document.querySelector('button[data-start]');
 
-let timerId = null;
+// let timerId = null;
 
-// button.setAttribute('disabled', 'disabled');
+// // button.setAttribute('disabled', 'disabled');
 
-input.addEventListener('click', onInput);
-button.addEventListener('click', onClickStartBtn);
+// input.addEventListener('click', onInput);
+// button.addEventListener('click', onClickStartBtn);
 
 
-function onInput(evt) {
+// function onInput(evt) {
     
-    const options = {
-        enableTime: true,
-        time_24hr: true,
-        defaultDate: new Date(),
-        minuteIncrement: 1,
-        onClose(selectedDates) {
-            console.log(selectedDates[0]);
-        },
-    };
-    
-    flatpickr("#datetime-picker", options);
-}
+// }
 
-function onClickStartBtn(evt) {
-    const startTime = Date.now();
+// function onClickStartBtn(evt) {
+//     const startTime = Date.now();
 
-    setInterval(() => {
-        const deltaTime = options - startTime;
-        const { days, hours, minutes, seconds } = convertMs(deltaTime);
-        console.log(`${days}:${hours}:${minutes}:${seconds}`);
-    }, 1000);
-}
+//     setInterval(() => {
+//         const deltaTime = options - startTime;
+//         const { days, hours, minutes, seconds } = convertMs(deltaTime);
+//         console.log(`${days}:${hours}:${minutes}:${seconds}`);
+//     }, 1000);
+// }
 
-function addLeadingZero(value) {
-    return String(value).padStart(2, '0');
-}
+// function addLeadingZero(value) {
+//     return String(value).padStart(2, '0');
+// }
 
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+// function convertMs(ms) {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
 
-  const days = addLeadingZero(Math.floor(ms / day));
-  const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+//   const days = addLeadingZero(Math.floor(ms / day));
+//   const hours = addLeadingZero(Math.floor((ms % day) / hour));
+//   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+//   const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
-  return { days, hours, minutes, seconds };
-}
+//   return { days, hours, minutes, seconds };
+// }

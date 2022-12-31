1b9db3f1abd74f79801543c1d8d1605f7f99bdf3
VitaBabenko <babenkovita1988@gmail.com>
2022-12-31 23:54:57 +0200

    changed js2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
 
 const options = {
   enableTime: true,
@@ -7,51 +8,50 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0] <= options.defaultDate) {
+      Notiflix.Notify.failure("Please choose a date in the future");
+    } else {
+      button.removeAttribute('disabled');
+    }
+    let selectedDate = selectedDates[0].getTime();
+    console.log('selectedDate', selectedDate);
   },
 };
 
 flatpickr("#datetime-picker", options);
 
-const input = document.querySelector('#datetime-picker');
 const button = document.querySelector('button[data-start]');
-
+button.setAttribute('disabled', 'true');
 // let timerId = null;
 
-// // button.setAttribute('disabled', 'disabled');
+button.addEventListener('click', onClickStartBtn);
 
-// input.addEventListener('click', onInput);
-// button.addEventListener('click', onClickStartBtn);
+function onClickStartBtn(evt) {
+  const startTime = Date.now();
+  console.log('startTime', startTime)
+  
+  setInterval(() => {
+    const deltaTime = options.selectedDate - startTime;
+    console.log(deltaTime)
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+    console.log(`${days}:${hours}:${minutes}:${seconds}`);
+  }, 1000);
+}
 
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
 
-// function onInput(evt) {
-    
-// }
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-// function onClickStartBtn(evt) {
-//     const startTime = Date.now();
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
-//     setInterval(() => {
-//         const deltaTime = options - startTime;
-//         const { days, hours, minutes, seconds } = convertMs(deltaTime);
-//         console.log(`${days}:${hours}:${minutes}:${seconds}`);
-//     }, 1000);
-// }
-
-// function addLeadingZero(value) {
-//     return String(value).padStart(2, '0');
-// }
-
-// function convertMs(ms) {
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-
-//   const days = addLeadingZero(Math.floor(ms / day));
-//   const hours = addLeadingZero(Math.floor((ms % day) / hour));
-//   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-//   const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
-
-//   return { days, hours, minutes, seconds };
-// }
+  return { days, hours, minutes, seconds };
+}

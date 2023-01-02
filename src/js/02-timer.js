49ca11b15645a2eb299ba378ac44ec056491042e
VitaBabenko <babenkovita1988@gmail.com>
2023-01-02 18:29:44 +0200

    changed js2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,7 +2,7 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import Notiflix from 'notiflix';
 
-let selectedDates = null;
+let selectedDate = null;
 
 const options = {
   enableTime: true,
@@ -13,35 +13,79 @@ const options = {
     if (selectedDates[0] <= options.defaultDate) {
       Notiflix.Notify.failure("Please choose a date in the future");
     } else {
+      selectedDate = selectedDates[0];
       button.removeAttribute('disabled');
     }
-    console.log(selectedDates[0]);
+    console.log('selectedDates[0]', selectedDates[0]);
   },
 };
 
-selectedDates = 
-
 flatpickr("#datetime-picker", options);
 
-
 const button = document.querySelector('button[data-start]');
-button.setAttribute('disabled', 'true');
+const timer = document.querySelector('.timer');
 
-// let timerId = null;
+const field = document.querySelectorAll('.field');
+const days = document.querySelector('span[data-days]');
+const hours = document.querySelector('span[data-hours]');
+const minutes = document.querySelector('span[data-minutes]');
+const seconds = document.querySelector('span[data-seconds]');
+const label = document.querySelectorAll('.label');
+
+
+
+timer.style.display = 'flex';
+timer.style.marginTop = '15px';
+field[0].style.fontSize = '35px';
+field[1].style.fontSize = '35px';
+field[2].style.fontSize = '35px';
+field[3].style.fontSize = '35px';
+
+field[0].style.marginRight = '15px';
+field[1].style.marginRight = '15px';
+field[2].style.marginRight = '15px';
+
+days.style.display = 'flex';
+hours.style.display = 'flex';
+minutes.style.display = 'flex';
+seconds.style.display = 'flex';
+
+label[0].style.fontSize = '13px';
+label[1].style.fontSize = '13px';
+label[2].style.fontSize = '13px';
+label[3].style.fontSize = '13px';
+
+label[0].style.textTransform = 'uppercase';
+label[1].style.textTransform = 'uppercase';
+label[2].style.textTransform = 'uppercase';
+label[3].style.textTransform = 'uppercase';
+
+label[0].style.fontWeight = '600';
+label[1].style.fontWeight = '600';
+label[2].style.fontWeight = '600';
+label[3].style.fontWeight = '600';
+
+
+button.setAttribute('disabled', 'true');
 
 button.addEventListener('click', onClickStartBtn);
 
 function onClickStartBtn(evt) {
   
-  setInterval(() => {
+  const intervalId = setInterval(() => {
     const startTime = Date.now();
-    console.log('startTime', startTime);
-
-console.log('selectedDates', selectedDates[0])
-    const deltaTime = options.selectedDates - startTime;
-    console.log(deltaTime)
+    // console.log('startTime', startTime);
+    const deltaTime = selectedDate - startTime;
+    // console.log(deltaTime);
     const { days, hours, minutes, seconds } = convertMs(deltaTime);
+
+    updateTimer({ days, hours, minutes, seconds });
+
     console.log(`${days}:${hours}:${minutes}:${seconds}`);
+    if (deltaTime < 1000) {
+      clearInterval(intervalId);
+      return;
+    }
   }, 1000);
 }
 
@@ -61,4 +105,9 @@ function convertMs(ms) {
   const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
+};
+
+function updateTimer({ days, hours, minutes, seconds }) {
+  timer.textContent = `${days}:${hours}:${minutes}:${seconds}`;
 }
+
